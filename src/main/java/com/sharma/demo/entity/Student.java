2fcb29031b8bd9e157a3195de7e58cfe51036bbe
sakshisharma2025 <sakshy9798@gmail.com>
2025-09-18 12:02:01 +0530

    Update Student.java

--- src/main/java/com/sharma/demo/entity/Student.java
+++ src/main/java/com/sharma/demo/entity/Student.java
@@ -42,15 +42,33 @@ public class Student {
     }
 
     // getters / setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() { 
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getFirstName() { 
+        return firstName; 
+    }
+    public void setFirstName(String firstName) { 
+        this.firstName = firstName;
+    }
 
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getLastName() {
+        return lastName; 
+    
+    
+    }
+    public void setLastName(String lastName) { 
+        this.lastName = lastName;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() {
+        return email; 
+    }
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
 }
